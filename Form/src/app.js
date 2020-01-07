bc6088e2ecfc5a4bd860c48157191c55f3cb5dd8
Ligalaiz <slavakuz88@gmail.com>
2020-01-07 21:33:12 +0300

    form validation

--- Form/src/app.js
+++ Form/src/app.js
@@ -1,1 +1,29 @@
-console.log("App working...");
+import "./styles.css";
+import { isValid } from "./utils";
+
+const form = document.getElementById("form");
+const input = form.querySelector("#question-input");
+const submitBtn = form.querySelector("#submit");
+
+form.addEventListener("submit", submitFormHandler);
+input.addEventListener("input", () => {
+  submitBtn.disabled = !isValid(input.value);
+});
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    };
+
+    submitBtn.disabled = true;
+    console.log("Question", question);
+
+    input.value = "";
+    input.className = "";
+    submitBtn.disabled = false;
+  }
+}
