e90c146bf9f37be7ec86e516e46fd700f685c1b2
Ligalaiz <slavakuz88@gmail.com>
2020-02-02 15:33:08 +0300

    Adding functionality

---
+++ debug/app.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let num1 = document.querySelector("#num1");
+  let num2 = document.querySelector("#num2");
+  let addBtn = document.querySelector("#add");
+  let subBtn = document.querySelector("#sub");
+  let output = document.querySelector("#output");
+
+  function getInputValues() {
+    let value1 = parseInt(num1.value);
+    let value2 = parseInt(num2.value);
+
+    return [value1, value2];
+  }
+
+  function addHandler() {
+    let values = getInputValues();
+    let result = values[0] + values[1];
+    displayResult(result);
+  }
+
+  function subHandler() {
+    let values = getInputValues();
+    let result = values[0] - values[1];
+    displayResult(result);
+  }
+
+  function displayResult(result) {
+    output.closest(".card").style.display = "block";
+    output.innerHTML = `Result = ${result}`;
+  }
+
+  addBtn.addEventListener("click", addHandler);
+  subBtn.addEventListener("click", subHandler);
+});
